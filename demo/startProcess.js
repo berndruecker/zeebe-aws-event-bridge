06a9c9f5757f9528575663d5b6fbd7c2b7268368
cornelius <cornelius.suermann@gmail.com>
2020-08-13 14:15:20 +0200

    fix: startProcess

--- demo/startProcess.js
+++ demo/startProcess.js
@@ -4,8 +4,9 @@ const { ZBClient, Duration } = require('zeebe-node')
 
 ;(async () => {
   const zbc = new ZBClient()
-  const result = await zbc.createWorkflowInstance('test', {
-    testData: 'something'
+  const result = await zbc.createWorkflowInstance('demo', {
+    testData: 'something',
+    myCurrentTime: new Date().toISOString()
   })
   console.log(result)
 })()
